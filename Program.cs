7783295852898f5c7556ddb29da1f0e559d1c4de
JairGRC <jgrodriguezcr@gmail.com>
2022-07-31 01:56:26 -0500

    Agregando clase StartUp, configuraciones mas ordenadas

--- Program.cs
+++ Program.cs
@@ -1,25 +1,12 @@
+using WebApiAutores;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var startUp = new StartUp(builder.Configuration);
+startUp.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+startUp.Configure(app,app.Environment);
 
 app.Run();
