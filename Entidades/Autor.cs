c469bd62d389dff1c3b4098da89e1a5df5843091
JairGRC <jgrodriguezcr@gmail.com>
2022-08-01 20:20:46 -0500

    Validaciones en Entidades

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -1,9 +1,23 @@
-﻿namespace WebApiAutores.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiAutores.Entidades
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(maximumLength:5,ErrorMessage ="El campo {0} no debe tener mas de {1} caracteres")]
         public string Nombre { get; set; }
+        [Range(18,120)]
+        [NotMapped]
+        public int Edad { get; set; }
+        [CreditCard]
+        [NotMapped]
+        public string TarjetaDeCredito { get; set; }
+        [Url]
+        [NotMapped]
+        public string URL { get; set; }
         public List<Libro> Libros { get; set; }
     }
 }
